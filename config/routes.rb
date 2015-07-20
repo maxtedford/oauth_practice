067655e757f5650bed8b8e740c07a844616003fc
Max Tedford <max.tedford@gmail.com>
2015-07-20 10:54:48 -0600

    add logout path with subsequent destroy method in sessions controller and logout path in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
-  get  '/dashboard', to: 'dashboard#show'
-  get  '/auth/github', as: :login
-  get  '/auth/github/callback', to: 'sessions#create'
+  get    '/dashboard', to: 'dashboard#show'
+  get    '/auth/github', as: :login
+  get    '/auth/github/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   root 'home#show'
 end
