063d785bf4c439868c5c2141e3f1e28168c552fc
Max Tedford <max.tedford@gmail.com>
2015-07-20 10:33:17 -0600

    update view and routes to create login route helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   get  '/dashboard', to: 'dashboard#show'
-  get  '/auth/github/callback', to: 'sessions#create', as: 'login'
+  get  '/auth/github', as: :login
+  get  '/auth/github/callback', to: 'sessions#create'
   root 'home#show'
 end
