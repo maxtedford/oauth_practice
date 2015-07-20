35e9c0db981d6d61bcdd861e68f788f66c764e51
Max Tedford <max.tedford@gmail.com>
2015-07-20 10:07:20 -0600

    add logic to new method that finds the user by the oauth info and sets all db attributes accordingly, then returns the new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   
   def self.find_or_create_from_oauth(oauth)
+    user = User.find_or_create_by(provider: oauth.provider, uid: oauth.uid)
     
+    user.email     = oauth.info.email
+    user.nickname  = oauth.info.nickname
+    user.image_url = oauth.info.image_url
+    user.token     = oauth.credentials.token
+    user.save
+    
+    user
   end
 end
