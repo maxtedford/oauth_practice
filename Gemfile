b0f6392cee5bca68b8b2ed6f699b47ff614717e5
Max Tedford <max.tedford@gmail.com>
2015-07-20 10:43:05 -0600

    install figaro, update omniauth.rb to hide secret keys, and changed keys so I could scope them into the application.yml file

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem 'omniauth-github'
+gem 'figaro'
 
 group :development, :test do
   gem 'better_errors'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, 'dd9f160cd70d18dafa4f', '007698e4d3b8aa1ac852daf3b62d8cdddf0670ff'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'] 
 end
